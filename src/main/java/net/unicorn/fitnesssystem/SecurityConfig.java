8ce32b635219aad7f18f050219567801f434b28e
stefanJambrich <nutela415@gmail.com>
2026-05-28 17:28:44 +0200

    Fixed db, added open-api generator

---
+++ src/main/java/net/unicorn/fitnesssystem/SecurityConfig.java
@@ -0,0 +1,21 @@
+package net.unicorn.fitnesssystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
